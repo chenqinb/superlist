b27272c83c0d25e67e08a7fd89fcbcb68c4883f3
chenqinbo <chenqinbo@null.com>
2015-08-14 21:25:56 +0800

    2 notes test

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,7 @@ module functional_tests.py
 '''
 #import builtin/3rd party/other ourself
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #global variables
@@ -43,7 +44,7 @@ class NewVistorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys("But peacock feather")
+        inputbox.send_keys("Buy peacock feather")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
@@ -51,13 +52,19 @@ class NewVistorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-                any(row.text == "1: Buy peacock feathers" for row in rows),
-		"New to-do item did not appear in table"
-                )
+        self.assertIn("1:Buy peacock feather", [row.text for row in rows])
+
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("1:Buy peacock feather", [row.text for row in rows])
+        self.assertIn("2:Use peacock feathers to make a fly", [row.text for row in rows])
+
         self.fail("Finish the test!")
 
         # The page updates again, and now shows both items on her list
