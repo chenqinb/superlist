0ceb878ff89203b2b4ca36a5b05df6b788383994
chenqinbo <chenqinbo@null.com>
2015-08-16 09:39:56 +0800

    test unique url

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -55,6 +55,8 @@ class NewVistorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
         self.check_for_row_in_list_table("1:Buy peacock feather")
 
         # There is still a text box inviting her to add another item. She
@@ -66,6 +68,32 @@ class NewVistorTest(LiveServerTestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table("1:Buy peacock feather")
         self.check_for_row_in_list_table("2:Use peacock feathers to make a fly")
+
+        # now for a new user, Francis, comes along to the site
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visit the home page,  There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        # Francis start a new list by entring a new item.
+        # He is less interesting then Edith...
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis get his own uinique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
