58abf0c17afdb2c5702849dcbc87f9128e6f77d6
chenqinbo <chenqinbo@null.com>
2015-08-25 21:52:31 +0800

    Refacotr list view to handle new item POSTs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -25,25 +25,25 @@ class ItemValidtionTest(FunctionalTest):
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
         error = self.browser.find_element_by_css_selector(".has-error")
-        self.assertEqual(error.text, "You can't have an empty list itme")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id("id_new_item").send_keys("Buy milk\n")
-        self.check_for_row_in_list_table("1. Buy milk")
+        self.check_for_row_in_list_table("1:Buy milk")
 
         # Perversely, she now decides to submit a second blank list item
         self.browser.find_element_by_id("id_new_item").send_keys("\n")
 
         # She receives a similar warning on the list page
-        self.check_for_row_in_list_table("1. Buy milk")
+        self.check_for_row_in_list_table("1:Buy milk")
         error = self.browser.find_element_by_css_selector(".has-error")
-        self.assertEqual(error.text, "You can't have an empty list itme")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
         # Edith goes to the home page 
         self.browser.find_element_by_id("id_new_item").send_keys("Make tea\n")
-        self.check_for_row_in_list_table("1. Buy milk")
-        self.check_for_row_in_list_table("2. Make tea")
+        self.check_for_row_in_list_table("1:Buy milk")
+        self.check_for_row_in_list_table("2:Make tea")
 
         self.fail("Finish the test!")
 
