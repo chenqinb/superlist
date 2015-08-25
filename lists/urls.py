91e1043f89267c92a9ceff672d7250c75434bd43
chenqinbo <chenqinbo@null.com>
2015-08-25 22:13:58 +0800

    refactor hard-coded URLs out of templates

--- lists/urls.py
+++ lists/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
-    url(r"^(\d+)/$", "lists.views.view_list", name="viw_list"),
+    url(r"^(\d+)/$", "lists.views.view_list", name="view_list"),
     url(r"^new$", "lists.views.new_list", name="new_list"),
 ]
