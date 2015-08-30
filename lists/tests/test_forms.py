2f299cd34803139520377dfcad52d57a770645ab
chenqinbo <chenqinbo@null.com>
2015-08-30 09:47:51 +0800

    chapter11 done

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -11,6 +11,7 @@ module test_forms.py
 #import builtin/3rd party/other ourself
 from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.models import Item, List
 
 #global variables
 
@@ -26,4 +27,12 @@ class ItemForTest(TestCase):
         form = ItemForm(data={"text": ""})
         self.assertFalse(form.is_valid())
         self.assertEqual( form.errors["text"], [EMPTY_ITEM_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data = {"text":"do me"})
+        new_item = form.save(for_list = list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, "do me")
+        self.assertEqual(new_item.list, list_)
 #function define
