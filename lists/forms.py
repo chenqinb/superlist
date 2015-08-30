396165b16ed89f161b1c191e2faf31ca799dc008
chenqinbo <chenqinbo@null.com>
2015-08-30 11:25:29 +0800

    take a validatoin error

--- lists/forms.py
+++ lists/forms.py
@@ -10,10 +10,12 @@ module forms.py
 '''
 #import builtin/3rd party/other ourself
 from django import forms
+from django.core.exceptions import ValidationError
 from lists.models import Item
 
 #global variables
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUMPLICATE_ITEM_ERROR = "You've alrealy got this in your list"
 
 #class define
 class ItemForm(forms.models.ModelForm):
@@ -33,6 +35,18 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list=for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {"text": [DUMPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
 
 
 #function define
