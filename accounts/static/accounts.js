67f80265d9ba05934919e3150ad75c89ce83f4b6
chenqinbo <chenqinbo@null.com>
2015-09-04 12:48:35 +0800

    login js test

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,7 +4,12 @@ var initialize = function(navigator, user, token, urls){
 		navigator.id.request();
 	});
 	navigator.id.watch({
-		loggedInUser: user
+		loggedInUser: user,
+		onlogin:function(assertion){
+			$.post(urls.login, 
+			{assertion:assertion, csrfmiddlewaretoken:token});
+		
+		}
 	});
 };
 window.Superlists = {
